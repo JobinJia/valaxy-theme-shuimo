ad2489790a25beaff1863dce2ee6778815559a29
Jobin <425605679@qq.com>
2026-05-15 19:56:50 +0800

    fix(a11y): swap goBack anchor for button in tags index page

--- theme/pages/tags/index.vue
+++ theme/pages/tags/index.vue
@@ -41,9 +41,9 @@ const tagList = computed(() => {
       </router-link>
     </div>
 
-    <a href="#" class="shuimo-tags-index__back" @click.prevent="goBack">
+    <button type="button" class="shuimo-tags-index__back" @click="goBack">
       {{ t('shuimo.back') }} ←
-    </a>
+    </button>
   </div>
 </template>
 
@@ -110,6 +110,10 @@ meta:
   }
 
   &__back {
+    border: 0;
+    background: none;
+    padding: 0;
+    cursor: pointer;
     margin-top: 40px;
     font-size: 13px;
     color: var(--sm-ink-light);
